17c2931573bf3728518bc434cc028e81959f474f
Smit <smit.satwara@fdmgroup.com>
2026-02-28 12:25:21 +0530

    initial: spring boot project setup with dependencies

---
+++ src/main/java/com/smitsatwara/cinebook/CinebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.smitsatwara.cinebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinebookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinebookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smitsatwara/cinebook/CinebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smitsatwara.cinebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinebookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
