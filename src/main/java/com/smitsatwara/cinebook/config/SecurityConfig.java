82bd9adb92043f81a5b9fa86abba5a4bad761fdd
Smit <smit.satwara@fdmgroup.com>
2026-03-09 14:39:21 +0530

    feat: add Swagger UI

--- src/main/java/com/smitsatwara/cinebook/config/SecurityConfig.java
+++ src/main/java/com/smitsatwara/cinebook/config/SecurityConfig.java
@@ -27,6 +27,12 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        //Swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         // Movies
                         .requestMatchers(HttpMethod.GET, "/api/movies/**").permitAll()
